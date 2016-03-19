8e94147a74c958a906972e53cb34d2340342a549
Chris Gonzales <gonzales.chris.a@gmail.com>
2016-03-18 21:20:03 -0400

    complete assignment 1

--- server.js
+++ server.js
@@ -2,4 +2,20 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+var path = require('path');
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './index.html'), function(err) {
+    if (err) res.status(500).send(err); 
+  }); 
+});
+
+app.get('/data', function(req, res) {
+  res.send(jsonData);
+});
+
+app.listen(3000);
