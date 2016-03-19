38fd73ab996ebdcc698674eae0db218ad9b90d3a
Chris Gonzales <gonzales.chris.a@gmail.com>
2016-03-18 23:07:24 -0400

    complete step 2

--- server/server.js
+++ server/server.js
@@ -17,11 +17,67 @@ app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
 var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res) {
+  // console.log(req);
+  res.status(200).type('json').send({
+    "desc": "returns all lions",
+    "data": lions
+  });
+});
+
+app.get('/lions/:id', function(req, res) {
+  console.log(req.params.id);
+  res.status(200).type('json').send({
+    "desc": "returns one lion represented by its id",
+    "data": _.find(lions, function(lion) {
+        return lion.id == req.params.id; 
+    }) || {}
+  });
+});
+
+app.post('/lions', function(req, res) {
+  // console.log(req);
+  var newLion = req.body;
+  newLion.id = id + '';
+  id++;
+  console.log(newLion);
+  lions.push(newLion);
+  res.status(200).type('json').json({
+    "desc": "create and returns a new lion using the posted object as the lion",
+    "data": newLion
+  });
+});
+
+app.put('/lions/:id', function(req, res){
+  var foundLion;
+  lions  = _.map(lions, function(lion) {
+    if (lion.id == req.params.id) {
+      foundLion = lion;
+      return req.body;
+    }
+    return lion;
+  });
+  res.status(200).type('json').send({
+    "desc": "updates and returns the matching lion with the posted update objec",
+    "data": foundLion || {}
+  });
+});
+
+app.delete('/lions/:id', function(req, res) {
+  var foundLion = _.remove(lions, function(lion) {
+    return lion.id = req.params.id;  
+  })[0];
+  res.status(200).type('json').send({
+    "desc": "deletes and returns the matching lion",
+    "data": foundLion || []
+  });
+
+});
+
 
 app.listen(3000);
 console.log('on port 3000');
